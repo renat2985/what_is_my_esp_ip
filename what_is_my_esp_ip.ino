8004e047463ff2ecefd9e19b5b3d9a5defb1587b
Renat2985 <wul@inbox.lv>
2021-08-19 16:58:15 +0000

    delete serial

--- what_is_my_esp_ip.ino
+++ what_is_my_esp_ip.ino
@@ -4,11 +4,11 @@ const char* ssid = "yourNetworkName";
 const char* password = "yourNetworkPassword";
 String url = "http://www.onclick.lv/esp/?mac="+WiFi.macAddress()+"&ip="+WiFi.localIP().toString();
 void setup () {
-  Serial.begin(115200);
+ // Serial.begin(115200);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.print("Connecting..");
+//    Serial.print("Connecting..");
   }
 }
 void loop() {
